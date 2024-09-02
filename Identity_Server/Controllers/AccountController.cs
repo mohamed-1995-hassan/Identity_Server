f21102e6afff2db29b51e3f423469ea25d323af9
ahmedabdo404 <ahmed.elzahed404@gmail.com>
2024-09-02 04:18:20 +0300

    add quickStart ui

--- Identity_Server/Controllers/AccountController.cs
+++ Identity_Server/Controllers/AccountController.cs
@@ -1,49 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Identity_Server.Models;
-using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
-using System.Numerics;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Identity_Server.Models;
+//using Microsoft.AspNetCore.Identity;
+//using System.Security.Claims;
+//using System.Numerics;
 
-namespace Identity_Server.Controllers
-{
-    public class AccountController : Controller
-    {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
+//namespace Identity_Server.Controllers
+//{
+//    public class AccountController : Controller
+//    {
+//        private readonly SignInManager<IdentityUser> _signInManager;
+//        private readonly UserManager<IdentityUser> _userManager;
 
-        public AccountController(SignInManager<IdentityUser> signInManager,UserManager<IdentityUser> userManager)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
-        {
-            return View(new LoginViewModel
-            {
-                ReturnUrl = returnUrl
-            });
-        }
+//        public AccountController(SignInManager<IdentityUser> signInManager,UserManager<IdentityUser> userManager)
+//        {
+//            _signInManager = signInManager;
+//            _userManager = userManager;
+//        }
+//        [HttpGet]
+//        public IActionResult Login(string returnUrl)
+//        {
+//            return View(new LoginViewModel
+//            {
+//                ReturnUrl = returnUrl
+//            });
+//        }
 
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel loginVm)
-        {
-            var user = new IdentityUser
-            {
-                UserName = loginVm.UserName
-            };
+//        [HttpPost]
+//        public async Task<IActionResult> Login(LoginViewModel loginVm)
+//        {
+//            var user = new IdentityUser
+//            {
+//                UserName = loginVm.UserName
+//            };
             
-            await _userManager.CreateAsync(user, loginVm.Password);
+//            await _userManager.CreateAsync(user, loginVm.Password);
     
-            var result = await _signInManager
-                    .PasswordSignInAsync(loginVm.UserName, loginVm.Password, false, false);
-            if (result.Succeeded)
-            {
-                return Redirect(loginVm.ReturnUrl);
-            }
+//            var result = await _signInManager
+//                    .PasswordSignInAsync(loginVm.UserName, loginVm.Password, false, false);
+//            if (result.Succeeded)
+//            {
+//                return Redirect(loginVm.ReturnUrl);
+//            }
                 
-            return View();
-        }
+//            return View();
+//        }
        
-    }
-}
+//    }
+//}
